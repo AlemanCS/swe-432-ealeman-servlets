69641b122110ec429a1386fdbe0cab95a9838940
Elias Aleman <aleman.music@gmail.com>
2020-12-15 17:30:50 -0500

    5th Iteration

--- src/main/java/servlet/SortFinal.java
+++ src/main/java/servlet/SortFinal.java
@@ -44,7 +44,7 @@ public void doPost (HttpServletRequest request, HttpServletResponse response)
    String operation = request.getParameter("Operation");
    String listOfString = request.getParameter("listOfStrings");
 
-   Stirng[] StringArray = listOfString.split(" ");
+   String[] StringArray = listOfString.split(" ");
 
    List<String> newStrings = new Arraylist<String>(Arrays.asList(StringArray));
 
