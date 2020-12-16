84b92df1ed18352e320c08a8dce6bbd7437aa83b
Elias Aleman <aleman.music@gmail.com>
2020-12-15 19:05:37 -0500

    Added 2nd server

--- src/main/java/servlet/SortFinal.java
+++ src/main/java/servlet/SortFinal.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 
 import javax.servlet.annotation.WebServlet;
 @WebServlet( name = "sortFinal", urlPatterns = {"/sortFinal"} )
@@ -22,7 +23,6 @@ public class SortFinal extends HttpServlet
 static String Domain  = "";
 static String Path    = "";
 static String Servlet = "sortFinal";
-
 
 static String OperationAscending = "Ascend";
 static String OperationDescending = "Descend";
--- src/main/java/servlet/sortFinal2.java
+++ src/main/java/servlet/sortFinal2.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 import javax.servlet.annotation.WebServlet;
 @WebServlet( name = "sortFinal2", urlPatterns = {"/sortFinal2"} )
 
