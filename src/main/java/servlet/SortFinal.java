945ee7ecb4135421a4eda07641ef3be8ea3f8b34
Elias Aleman <aleman.music@gmail.com>
2020-12-15 20:07:40 -0500

    Added 2nd server

--- src/main/java/servlet/SortFinal.java
+++ src/main/java/servlet/SortFinal.java
@@ -99,16 +99,15 @@ private void PrintBody (PrintWriter out)
 
 private void PrintTail (PrintWriter out)
 {
-   out.println("");
-   out.println("");
-   out.println("");
-   out.println("");
-   out.println(" <br>");
-   out.println(" <br>");
-
-   out.println("<h4>  SWE 432 Fall 2020 Final </h4>");
-   out.println("<h4> Elias Aleman </h4>");
-   out.println("</html>");
+    out.println("");
+    out.println("<br>");
+    out.println("<br>");
+    out.println("<br>");
+    out.println("<br>");
+    out.println("<hr>");
+    out.println("<h4 >  SWE 432 Fall 2020 Final </h4>");
+    out.println("<h4> Elias Aleman </h4>");
+    out.println("</html>");
 }
 
 } 
